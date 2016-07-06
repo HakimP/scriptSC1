ce6b392e9d247926ef4f900ebb08c214de6d4b50
HakimP <pedrohakim0@gmail.com>
2016-07-05 21:24:31 -0500

    Ejemplo propt js Actualizado

--- js/script.js
+++ js/script.js
@@ -8,3 +8,62 @@ function validaContrasenia() {
 	document.getElementById("coco").innerHTML =
         	"ok tu contraseña es: " + contrasenia;
 }
+
+function validaEntero() {
+	var pass="1234";
+	do {
+		var contrasenia = prompt("Valida que Sea un Número", "Número");
+	}while(isNaN(contrasenia));
+	document.getElementById("coco2").innerHTML =
+        	"ok si eres un número: " + contrasenia;
+}
+
+function validaEntero3() {
+	var pass="1234";
+	while(isNaN(contrasenia)) {
+		var contrasenia = prompt("Valida que Sea un Número", "Número");
+	};
+	document.getElementById("coco3").innerHTML =
+        	"ok tu contraseña es: " + contrasenia;
+}
+
+function sumaNumeros() {
+	var numero = prompt("Valida que Sea un Número", "Número");
+	var sumatoria = 0;
+
+	if(!isNaN(numero)) {
+		for(var i = 0 ; i<=numero ; i ++) {
+			sumatoria +=i;
+			console.log(sumatoria);
+		}
+	} else {
+		console.log("No es un Número");
+	}
+}
+
+function sumaNumeros2() {
+	var numero = prompt("Valida que Sea un Número", "Número");
+	var numero1 = 0;
+	var contador = 0;
+	while(contador <= numero) {
+		console.log("el contador va en " +contador);
+		numero1 = numero1 + contador;
+		console.log("variable de sumatoria va en:: " + numero1);
+		contador = contador + 1;
+	}
+}
+
+function serieFibonachi() {
+	var numero = prompt("Valida que Sea un Número", "Número");
+    var var1 = 0;
+    var var2 = 1;
+    var var3;
+    console.log(var1);
+    console.log(var2);
+    for(var i=3; i <= numero;i++){
+        var3 = var1 + var2;
+        var1 = var2;
+        var2 = var3;
+        console.log(var3);
+    }
+}
